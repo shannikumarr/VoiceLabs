28a5ebed3b16e1be80b894bcc7be37f887e04d1e
don <k9850942@gmail.com>
2026-02-09 16:12:06 +0530

    card added

--- src/script.js
+++ src/script.js
@@ -1,8 +1,14 @@
 // Get the speech synthesis instance
 const synth = window.speechSynthesis;
-let list = document.getElementById('list')
-let text = document.getElementById('text')
+let profile = document.querySelector('.profile')
+let pic = document.getElementsByClassName('pic')
 // This will store the voices
+// < div class="pic" >
+//     <p id="name">name</p>
+//     <p id="country">cou</p>
+//     <p id="lang">lang</p>
+//     <span class="dot"></span>
+//   </div >
 let voices = [];
 
 
@@ -11,25 +17,40 @@ let voices = [];
 function loadVoices() {
     voices = synth.getVoices();
 
+    voices.forEach((e) => {
+        let name = e.name.split(' ')[1]
+        let lang = e.name.split(' ')[5]
+        let country = e.name.split(' ').splice(6)
+
+
+        let div = document.createElement('div')
+        div.classList.add('pic')
+        let p1 = document.createElement('p')
+        let p2 = document.createElement('p')
+        let p3 = document.createElement('p')
+        let span = document.createElement('span')
+        span.classList.add('dot')
+
+        p1.textContent = `${name}`
+        p3.textContent = `${country}`
+        p3.textContent = `${lang}`
+
+
+        div.append(p1, p2, p3, span)
+
+
+        profile.appendChild(div)
 
 
 
-    for (const key of voices) {
 
-
-        let option = document.createElement('option')
-        option.value = `${key.lang}`
-        option.textContent = `${key.name}`
-        list.appendChild(option)
+    })
 
 
 
 
 
 
-
-
-    }
 
 
 
